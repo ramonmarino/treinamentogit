07d236c744e15a13481caa00ea51cb164b952f89
Ramon Marino <ramonmarino12@gmail.com>
2025-03-25 13:56:35 -0300

    testando comportamento

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+print("Hello World!")
+
+
+def print_hi(name):
+
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
